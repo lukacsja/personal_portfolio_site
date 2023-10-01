66f06847fd826ef43e0deb11bf9fd03559ad0159
Janos Lukacs <pszichae@gmail.com>
2023-10-02 01:16:58 +0200

    footer - added option to download CV

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,12 +8,12 @@ import Link from 'next/link';
 const Footer = () => {
   return (
     <footer className='box-border flex h-[48px] w-full items-center justify-between border-t border-lines bg-inherit pl-[18px]'>
-      <span className='text-secondary-gray'>find me in:</span>
-      <div className='flex'>
+      <div className='flex items-center justify-center'>
+        <span className='pr-[18px] text-secondary-gray'>find me in:</span>
         <Link
           href={contactsData.github.url}
           target='_blank'
-          className='flex h-12 w-12 items-center justify-center border-l border-lines transition-all duration-300 hover:bg-lines'
+          className='flex h-12 w-12 items-center justify-center border-x border-lines transition-all duration-300 hover:bg-lines'
         >
           <Image
             src={contactsData.github.icon}
@@ -25,7 +25,7 @@ const Footer = () => {
         <Link
           href={contactsData.linkedin.url}
           target='_blank'
-          className='flex h-12 w-12 items-center justify-center border-l border-lines transition-all duration-300 hover:bg-lines'
+          className='flex h-12 w-12 items-center justify-center border-r border-lines transition-all duration-300 hover:bg-lines'
         >
           <Image
             src={contactsData.linkedin.icon}
@@ -35,6 +35,15 @@ const Footer = () => {
           />
         </Link>
       </div>
+      <Link
+        href='/janos-lukacs-cv-front-end.pdf'
+        download
+        target='_blank'
+        className='flex h-12 w-12 items-center justify-center gap-[10px] border-l border-lines text-secondary-gray transition-all duration-300 hover:bg-lines lg:w-auto lg:px-[20px]'
+      >
+        <Image src='/icons/cv.svg' alt='cv logo' width={24} height={24} />
+        <span className='hidden lg:block'>download-cv</span>
+      </Link>
     </footer>
   );
 };
